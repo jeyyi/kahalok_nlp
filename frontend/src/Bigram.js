76b170eb9e164f238f6a13559cc41f35d112f60e
Jayrhald Padilla <jayrhald.jp@gmail.com>
2022-12-01 17:24:36 +0800

    Bigram photo Sample

--- frontend/src/Bigram.js
+++ frontend/src/Bigram.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
 import Neck from "./components/Neck";
+import PBigram from "./Assets/bigram.png";
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
+
+
 const Bigram = () => {
   return (
     <div className="bg-gray-200">
@@ -8,12 +13,18 @@ const Bigram = () => {
       <Neck tempTitle={"Bigram Analysis"}></Neck>
       <div className="px-32 h-screen">
         <div className="flex flex-row h-2/3 bg-white rounded-b-xl p-2">
-          <div className="flex bg-gray-200 w-4/5 h-full">
+          <div className="flex w-4/5 h-full bg-white">
+            <Zoom>
+            <img src={PBigram} alt="Bigram" width={650}/></Zoom>
             {/* insert bigram image here */}
           </div>
           <div className="flex flex-col w-1/5 px-4">
-            <button className="bg-blue-900 text-white w-full h-14 text-2xl font-bold">Retrain</button>
-            <button className="bg-orange-600 text-white w-full h-14 text-2xl font-bold">Apply</button>
+            <button className="bg-blue-900 text-white w-full h-14 text-2xl font-bold">
+              Retrain
+            </button>
+            <button className="bg-green-600 text-white w-full h-14 text-2xl font-bold">
+              Download
+            </button>
           </div>
         </div>
       </div>
