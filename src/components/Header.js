79579a9e876c3733298a70ccc1ad24dc21023e6a
Jayrhald Padilla <jayrhald.jp@gmail.com>
2023-04-27 11:06:22 +0800

    finalization without sentiment analysis

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ export default function Navbar({ fixed }) {
               className="text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
               href="/"
             >
-              kalahok
+              bosesko
             </a>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
